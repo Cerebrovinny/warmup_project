49017e0737730551206b3b15a32ad553627e9d05
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-02-04 07:51:10 -0300

    added cucumber divide test

---
+++ frontend/tests/e2e/world/calculatorWorld.ts
@@ -0,0 +1,9 @@
+import { Calculator } from "../src/calculator";
+import { World } from "@cucumber/cucumber";
+
+declare module "@cucumber/cucumber" {
+    interface World {
+        calculator: Calculator;
+        actual: number;
+    }
+}
